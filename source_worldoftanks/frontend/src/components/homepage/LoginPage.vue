4a7edddbd25a6d6848536e61ef907b881d13a6b6
Branko Gluvajić <gluvajicbranko@gmail.com>
2020-04-17 22:38:27 +0200

    Frontend 1.0

---
+++ source_worldoftanks/frontend/src/components/homepage/LoginPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-card-group>
+        <b-card border-variant="light" style="max-width: 24rem;" class="mb-2 mx-auto">
+            <b-form-group
+                label-for="input-username"
+            >
+                <b-form-input id="input-username" v-model="username" placeholder="Username"></b-form-input>
+            </b-form-group>  
+
+            <b-form-group
+                label-for="input-pass"
+            >
+                <b-form-input id="input-pass" type="password" v-model="password" placeholder="Password"></b-form-input>
+            </b-form-group>
+        </b-card>
+    </b-card-group>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+
+</style>
+
---
+++ source_worldoftanks/frontend/src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+import LoginPage from '@/components/homepage/LoginPage'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    
+    routes: [
+        {
+            path: '/',
+            component: LoginPage
+        }
+    ]
+})
