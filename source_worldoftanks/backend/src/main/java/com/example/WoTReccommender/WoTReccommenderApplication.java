ab193e3560c54a3c06c1d0fa3d26abfc17cd4353
markosimanic <marezver023@gmail.com>
2020-04-24 22:18:40 +0200

    initial backend project

--- source_worldoftanks/backend/src/main/java/com/example/WoTReccommender/WoTReccommenderApplication.java
+++ source_worldoftanks/backend/src/main/java/com/example/WoTReccommender/WoTReccommenderApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.WoTReccommender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class WoTReccommenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WoTReccommenderApplication.class, args);
 	}
 
 }
